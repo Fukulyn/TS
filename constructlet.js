1084df0fb2388fe171b14e3503c9c4d400fd6a8b
Fukulyn <a098838628@gmail.com>
2024-10-25 15:39:24 +0800

    D

---
+++ constructlet.js
@@ -0,0 +1,5 @@
+var age = 25;
+var name = "Alice";
+name = "Bob"; // 
+console.log(age); // 25
+console.log(name); // Bob
---
+++ constructlet.ts
@@ -0,0 +1,8 @@
+const age: number = 25;
+
+let name: string = "Alice";
+
+name = "Bob"; // 
+
+console.log(age); // 25
+console.log(name); // Bob
